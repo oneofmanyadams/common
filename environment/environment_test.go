0a73eb91510dba7044dca1313207d7d51acfe2bb
Adam P <adam.plazewski@gmail.com>
2021-09-05 00:27:59 -0600

    fixed some example syntax

--- environment/environment_test.go
+++ environment/environment_test.go
@@ -20,13 +20,11 @@ func ExampleNewEnvironment() {
 
 func ExampleNewDefaultEnvironment() {
     // assuming the user's HomeDir is C:\Users\Me
-    env, env_archive, env_data, env_settings := environment.NewDefaultEnvironment("mainAppFolder")
-    fmt.Println(env.FullPath(env))
+    env, env_archive, env_data, env_settings := NewDefaultEnvironment("mainAppFolder")
     fmt.Println(env.FullPath(env_archive))
     fmt.Println(env.FullPath(env_data))
     fmt.Println(env.FullPath(env_settings))
-    // Output: C:\Users\Me\mainAppFolder
-    // C:\Users\Me\mainAppFolder\archive
+    // Output: C:\Users\Me\mainAppFolder\archive
     // C:\Users\Me\mainAppFolder\data
     // C:\Users\Me\mainAppFolder\settings
 }
