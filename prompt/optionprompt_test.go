724b916c91dbd56f099714fe0b37980554db17a9
Adam P <adam.plazewski@gmail.com>
2021-11-26 23:32:33 -0700

    fixing some weird formatting issues in the Prompt example.

--- prompt/optionprompt_test.go
+++ prompt/optionprompt_test.go
@@ -9,7 +9,7 @@ import (
 ////////////////////////////////////////////////////////////////////////////////
 
 func ExampleOptionPrompt() {
-    // Quick little hack for abusing for loops to have user prompts flow
+	// Quick little hack for abusing for loops to have user prompts flow
 	// with the program code (no need to split user prompting into separate
 	// program logic). If the user does not provide an answer that matches an
 	// option then the user is prompted again.
@@ -17,16 +17,16 @@ func ExampleOptionPrompt() {
 	// associated with the option (so for example: the first option is
 	// selected by the user entering 0). 
 	for p := New("Do what?"); p.Ask; p.PromptUser() {
-        
-		if p.Option("First thing") {
-            fmt.Println("One")
-        }
-        
-		if p.Option("Second thing") {
-            fmt.Println("Two")
-        }
 
-    }
+		if p.Option("First thing") {
+			fmt.Println("One")
+		}
+
+		if p.Option("Second thing") {
+			fmt.Println("Two")
+		}
+
+	}
 	//Output: Do what?
 	// 0 First thing
 	// 1 Second thing
